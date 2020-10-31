7f9f489fb7436232d68641f24ccc195a95a2f5c9
keix1 <xkei4dev@gmail.com>
2020-10-31 11:03:00 +0900

    Hide info env

--- spread_sheet.py
+++ spread_sheet.py
@@ -1,5 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SHEET_URL = os.environ.get('SHEET_URL')
 
 def post_sensor_to_spreadsheet(humidity, temperature):
-    url = f"https://script.google.com/macros/s/xxxxxxxxx/exec?humidity={humidity}&temperature={temperature}"
+    url = f"https://script.google.com/macros/s/{SHEET_URL}/exec?humidity={humidity}&temperature={temperature}"
     requests.get(url)
